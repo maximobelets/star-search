1458ecad513946ad190fb1ae9f6a77acaffc87bb
Maxim Obelets <maximobelets@gmail.com>
2021-08-19 15:26:28 +0500

    Clean project

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './index.css';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
